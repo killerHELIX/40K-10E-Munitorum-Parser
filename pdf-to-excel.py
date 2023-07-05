83dcde56886bac21a7714a8cfe4909e4a8800f30
Ryan Harris <ryanh96@me.com>
2023-07-05 17:53:39 -0400

    Fix enhancements

--- pdf-to-excel.py
+++ pdf-to-excel.py
@@ -146,22 +146,17 @@ def create_datamodel_2(reader):
         "World Eaters"
     ]
 
+    # Lines that contain any of these strings will be completely ignored.
     IGNORED_LINES = [
         "FORGE WORLD POINTS VALUES",
-        "DETACHMENT ENHANCEMENTS"
     ]
 
     # Initialize faction data structures.
     model = {}
-    # model["factions"] = ALL_FACTIONS
-    # for faction in model["factions"]:
-    #     faction_name = faction
-    #     faction = model["factions"].get(faction)
-    #     faction["units"] = {}
-    #     faction["enhancements"] = {}
     model["factions"] = [ {"name": faction, "units": [], "enhancements": [] } for faction in ALL_FACTIONS ] 
 
-    current_faction_name = None
+    current_faction_name = None # Set this outside of the page loop to keep state across pages.
+
     for i in range(0, len(reader.pages)):
         if i == 0: continue # Skip title page.
 
@@ -169,62 +164,65 @@ def create_datamodel_2(reader):
         text = page.extract_text()
         lines = text.split("\n")
         page_title = lines[0].strip().title()
-        current_faction_name = page_title if page_title in [ faction["name"] for faction in model["factions"] ] else current_faction_name
-        current_faction = [ faction for faction in model["factions"] if faction["name"] == current_faction_name ][0]
+        current_faction_name = page_title if page_title in [ faction["name"] for faction in model["factions"] ] else current_faction_name # Current faction is whatever the last valid page title was.
+        current_faction = [ faction for faction in model["factions"] if faction["name"] == current_faction_name ][0] # Object reference of the faction name found above.
         lines.pop() # Remove page number.
         lines.pop(0) # Remove title.
 
-        if current_faction_name == "Adeptus Custodes":
+        if current_faction_name == "Adeptus Custodes": # Temporarily only generate for a certain faction.
             print(f"Current faction: {current_faction_name}")
-            # pprint(lines)
 
+            # Default unit data structure.
             current_unit = {
                 "name": None,
                 "composition": []
             }
 
-            is_enhancement = False
+            is_enhancement = False # Enhancements treat their 'composition' differently.
 
             for line in lines:
 
-                is_enhancement = "DETACHMENT ENHANCEMENTS" in line or is_enhancement
+                print(line)
 
+                is_enhancement = "DETACHMENT ENHANCEMENTS" in line or is_enhancement
                 if line in IGNORED_LINES: continue
 
-                print(line)
-                if re.search(r"[.]+", line):
-                    # print(f"{line} has dots in it")
-                    if not line.endswith("pts"):
-                        # line += " INVALID"
+                # The "DETACHMENT ENHANCEMENTS" line indicates only Enhancements will be added from this point on, so add & clear the current unit before doing that.
+                # Then skip this step of the loop because we don't care about "DETACHMENT ENHANCEMENTS" otherwise. (continue)
+                if "DETACHMENT ENHANCEMENTS" in line:
+                    current_unit = add_unit_and_clear(current_unit, current_faction["units"])
+                    continue
+
+                if re.search(r"[.]+", line): # Matches the general 'x models ........... 1234 pts' pattern.
+                    if not line.endswith("pts"): # Sometimes a parsing error can result in 'x models ...... 1234 ptsNew Unit Name'. Detect this and add & clear before continuing.
                         split = re.split(r"pts", line)
                         current_unit["composition"].append(split[0])
-                        current_unit = add_unit_and_clear(current_unit, current_faction["units"] if not is_enhancement else current_faction["enhancements"])
+                        current_unit = add_unit_and_clear(current_unit, current_faction["units"])
                         current_unit["name"] = split[1]
                     else:
-                        current_unit["composition"].append(line)
 
-                else:
-                    # print(f"{line} NO dots in it")
-                    if current_unit["name"]:
+                        if is_enhancement: # Enhancements set their 'name' to the first half of their line and their 'composition' to the last half.
+                            split = re.split(r"[.]+", line)
+                            current_unit["name"] = split[0].strip()
+                            current_unit["composition"].append(split[1].strip())
+                            current_unit = add_unit_and_clear(current_unit, current_faction["enhancements"])
+                        else: # Not an enhancement so just add the entire line as is into the composition.
+                            current_unit["composition"].append(line)
+
+                else: # Doesn't match the 'x models ..... 1234 pts' pattern - that means this must be the name of a unit.
+                    if current_unit["name"]: # If the current unit already has a 'name' key, then this is the name of the next unit. So add & clear.
                         current_unit = add_unit_and_clear(current_unit, current_faction["units"] if not is_enhancement else current_faction["enhancements"])
 
-                    current_unit["name"] = line
-
-                # print("Current Unit")
-                # pprint(current_unit)
-
-                # if "FORGE WORLD" in line: break
-            current_unit = add_unit_and_clear(current_unit, current_faction["units"]) # Add & clear the final unit on the page.
-                
-                        
-            # print("Current Faction")
-            # pprint(current_faction)
+                    current_unit["name"] = line # Then, add the line to the 'name' key.
 
     return model
 
 def add_unit_and_clear(unit, units):
+    """
+    Adds a provided unit to the provided list 'units', then returns a "cleared" (no data) unit.
+    """
     units.append(unit)
-    # print(f"Adding {unit['name']} to units")
+    print(f"Adding {unit['name']}")
 
     # Clear current unit.
     return {
